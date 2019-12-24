f1c300d091cf5a6fe2fa8cb057948f765afae676
ykw <1794093413@qq.com>
2019-12-25 00:00:06 +0800

    小修改

--- Simple algorithm/冒泡排序.js
+++ Simple algorithm/冒泡排序.js
@@ -2,8 +2,8 @@
 function maopao(arr){
     let newArr = []
     let temp = 0
-    for(let i=0;i<=arr.length-1;i++){
-        for(let j = 0;j<=arr.length-i-1;j++){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j = 0;j<arr.length-i-1;j++){
             if(arr[j]<arr[j+1]){
                 temp = arr[j]
                 arr[j] = arr[j+1]
