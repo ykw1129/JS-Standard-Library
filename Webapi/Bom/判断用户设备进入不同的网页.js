2d7a5a848367af7951e704c9f69738ca1224f24a
ykw <1794093413@qq.com>
2019-12-27 15:14:22 +0800

    location、novigator、history

---
+++ Webapi/Bom/判断用户设备进入不同的网页.js
@@ -0,0 +1,5 @@
+if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+    window.location.href = "";//手机
+}else{
+    window.location.href = "";//电脑
+}
