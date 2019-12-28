374737edc87637ef46e9bf67826fcf5b26922749
ykw <1794093413@qq.com>
2019-12-28 23:25:46 +0800

    Funtion的一些注意事项

---
+++ Function/定义函数的三种方式.js
@@ -0,0 +1,10 @@
+// 1.命名函数
+function a(){
+
+}
+// 2.匿名函数
+var b = function(){
+
+}
+// 3.Function实例
+var c = new Function()
---
+++ Function/调用函数的方式.js
@@ -0,0 +1,22 @@
+// 1.普通函数
+    function fn(){
+
+    }
+    // fn() 或者 fn.call()
+    // 2.对象的方式
+    /* var a = {
+        say:function(){
+
+        }
+    }
+    a.say() 
+    */
+//    3.构造函数
+/* function Star(){}
+new Star() */
+    // 4.绑定事件函数
+    // btn.onclick = function(){}
+    // 5.定时器
+    // setInterval(functio(){},1000)
+    // 6.立即执行函数
+    // (function(){})()
