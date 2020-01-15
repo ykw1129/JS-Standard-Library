10b3c716e19e8f11d574b3b32306613e36e78c91
ykw1129 <1794093413@qq.com>
2020-01-15 16:04:02 +0800

    mongoose connect

--- Nodejs/mongodb/model.js
+++ Nodejs/mongodb/model.js
@@ -1,1 +1,7 @@
 const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/mongodbtest',{useNewUrlParse:true})
+    .then((result) => {
+        console.log("数据库连接成功")
+    }).catch((err) => {
+        console.log("数据库连接失败")
+    });
