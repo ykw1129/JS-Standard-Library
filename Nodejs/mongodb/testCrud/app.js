25e32be5dbdebafce88d01c7af6b8437ac688f34
ykw1129 <1794093413@qq.com>
2020-02-03 10:16:09 +0800

    字符串拼接

--- Nodejs/mongodb/testCrud/app.js
+++ Nodejs/mongodb/testCrud/app.js
@@ -5,36 +5,111 @@
 // 当用户访问/add时，呈现表单页面，并实现添加用户信息功能
 // 当用户访问/modify时，呈现修改页面，并实现修改用户信息功能
 // 当用户访问/delete时，实现用户删除功能
-const http =require('http');
+const http = require('http');
+const url = require('url')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/playground',{ useUnifiedTopology: true,useNewUrlParser: true })
-    .then(()=>console.log('数据库连接成功'))
-    .catch((err)=>console.log(err+'数据库连接失败'))
+mongoose.connect('mongodb://localhost/playground', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    .then(() => console.log('数据库连接成功'))
+    .catch((err) => console.log(err + '数据库连接失败'))
 
 // 创建用户集合规则
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        minlength:2,
-        maxlength:20
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 20
     },
-    age:{
-        type:Number,
-        min:18,
-        max:80,
+    age: {
+        type: Number,
+        min: 18,
+        max: 80,
     },
-    password:String,
-    email:String,
-    hobbies:[String]
+    password: String,
+    email: String,
+    hobbies: [String]
 })
-const User = mongoose.model('User',userSchema)
+const User = mongoose.model('User', userSchema)
 // 创建服务器
 const app = http.createServer();
 
-// 为服务器对象添加请求事件
-app.on('request',(req,res)=>{
+app.on('request', async (req, res) => {
+    // 请求方式
+    const method = req.method;
+    // 请求地址
+    const {
+        pathname
+    } = url.parse(req.url)
+    // 判断请求方式
+    if (method == 'GET') {
+
+        if (pathname == '/list') {
+            // 呈现用户列表页面
+            let users = await User.find();
+            console.log(users)
+            let list = `<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+            <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
+                integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        </head>
+        
+        <body>
+        
+        <div class="container">
+            <h6>
+                <a href="add.html" class="btn btn-primary">添加用户</a>
+            </h6>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>用户名</th>
+                        <th>年龄</th>
+                        <th>爱好</th>
+                        <th>邮箱</th>
+                        <th>操作</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>张三</td>
+                        <td>20</td>
+                        <td>
+                            <span>sing</span>
+                            <span>dance</span>
+                        </td>
+                        <td>123@123.com</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+            
+        </div>
+        
+        </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+        </script>
+        
+        </html>
+        `;
+            res.end(list)
+        }
+
+    } else if (method == 'POST') {
+
+    }
     res.end('ok')
 })
+// 为服务器对象添加请求事件
 
 app.listen(3000)
