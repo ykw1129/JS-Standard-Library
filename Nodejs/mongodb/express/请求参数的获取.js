7fa19a4c4ccd13345b63150751f42788a0f8573f
ykw1129 <1794093413@qq.com>
2020-02-13 10:02:52 +0800

    get请求参数的获取

--- Nodejs/mongodb/express/请求参数的获取.js
+++ Nodejs/mongodb/express/请求参数的获取.js
@@ -2,5 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/index',(req,res)=>{
+    // 获取get请求参数
+    res.send(req.query)
+})
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
