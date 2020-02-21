475d51c39c94cfcc2723d9c46030b66f0089e662
ykw1129 <1794093413@qq.com>
2020-02-21 10:36:51 +0800

    添加标识

--- Nodejs/mongodb/blog/router/admin/userPage.js
+++ Nodejs/mongodb/blog/router/admin/userPage.js
@@ -1,6 +1,8 @@
 const {User}= require('../../model/user')
 
 module.exports =async (req,res)=>{
+    // 标识 标识当前访问的是用户管理页面
+    req.app.locals.currentLink = 'user'
     // 接收客户端传递过来的当前页参数
     let page = req.query.page||1
     // 每一页显示数据条数
