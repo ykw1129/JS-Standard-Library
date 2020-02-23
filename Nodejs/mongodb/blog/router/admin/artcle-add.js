f126340789d3cd36118f3d230d8885883c94b504
ykw1129 <1794093413@qq.com>
2020-02-23 13:08:06 +0800

    新增formidable模块

--- Nodejs/mongodb/blog/router/admin/artcle-add.js
+++ Nodejs/mongodb/blog/router/admin/artcle-add.js
@@ -1,3 +1,15 @@
+// 引入formidable第三方模块
+const formidable =  require('formidable')
+const path = require('path')
+
 module.exports = (req,res) =>{
+    // 1、创建表单解析对象
+    const form = new formidable.IncomingForm();
+    // 2.配置上传文件的存放位置
+    form.uploadDir = path.join(__dirname,'../','../','public','uploads')
+    // 3、保留上传文件的后缀
+    form.keepExtensions = false
+    // 4、解析表单
+    form
     res.send('ok')
 }
