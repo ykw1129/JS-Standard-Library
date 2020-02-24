8ba6a13ee180db08146317a7b18766db995b0500
ykw <1794093413@qq.com>
2020-02-24 15:29:03 +0800

    文章分页

--- Nodejs/mongodb/blog/router/admin/article.js
+++ Nodejs/mongodb/blog/router/admin/article.js
@@ -1,6 +1,8 @@
 const {Article} = require('../../model/article')
 const pagination = require('mongoose-sex-page')
 module.exports = async (req,res)=>{
+    // 接受客户端传递的页码
+    const {page} = req.query
     // 标识 标识当前访问的是用户管理页面
     req.app.locals.currentLink = 'article'
     let articles = await pagination(Article).find().page(1).size(2).display(3).populate('author').exec()
@@ -8,6 +10,8 @@ module.exports = async (req,res)=>{
     // size 指定每页显示的数据
     // display 指定客户端要显示的页码数量
     res.render('admin/article',{
-        articles
+        articles,
+        page:parseInt(page),
+        max:Math.max.apply(null,articles.display)
     })
 }
