b62056a82ccaf29051c63225552a351721e45c37
ykw <1794093413@qq.com>
2020-02-25 10:18:46 +0800

    主页的设计

--- Nodejs/mongodb/blog/router/home/index.js
+++ Nodejs/mongodb/blog/router/home/index.js
@@ -3,11 +3,12 @@ const pagination = require('mongoose-sex-page')
 module.exports = async (req,res)=>{
     const size = 6
     const tetol = await Article.find()
-    const {page} = req.query
+    const {page} = req.query||1;
     const display = tetol/size
     let articles = await pagination(Article).find().page(page).size(size).display(display).populate('author').exec()
-    res.render('home/article',{
+    res.render('home/index',{
         articles,
-        display
+        display,
+        page
     })
 }
