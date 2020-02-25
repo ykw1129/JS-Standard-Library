ee66980aa73b2b366d63c78d9d286842a59a0e1f
ykw <1794093413@qq.com>
2020-02-25 23:49:28 +0800

    评论数据模型

--- Nodejs/mongodb/blog/router/home/article.js
+++ Nodejs/mongodb/blog/router/home/article.js
@@ -1,5 +1,11 @@
-module.exports = (req,res)=>{
-    res.render('home/article')
+const {Article} = require('../../model/article')
+module.exports =async (req,res)=>{
+    const id = req.query.id;
+    // 根据id查询文章详细信息
+    let article = await Article.findOne({_id:id}).populate('author')
+    res.render('home/article',{
+        article
+    })
 
-    
+
 }
