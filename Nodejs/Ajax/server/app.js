09618a9edb31e2262b31850247822d39ef2543cd
ykw <1794093413@qq.com>
2020-02-27 14:27:18 +0800

    ajax获取的数据 渲染到页面

--- Nodejs/Ajax/server/app.js
+++ Nodejs/Ajax/server/app.js
@@ -9,4 +9,8 @@ app.get('/first',(req,res)=>{
     res.send('hello ajax')
 })
 
+app.get('/responseData',(req,res)=>{
+    res.send({"name":"zs"})
+})
+
 app.listen(port, () => console.log(`服务器启动了`))
