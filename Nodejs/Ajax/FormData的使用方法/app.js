2a85b7e9454c001c4db935ec8ce03e848b5edd8d
ykw <1794093413@qq.com>
2020-03-01 15:49:13 +0800

    formdata实例

---
+++ Nodejs/Ajax/FormData的使用方法/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const formidable = require('formidable')
+// const bodyParser = require('body-parser')
+const template = require('art-template')
+const fs = require('fs')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname,'public')))
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended:true}))
+// 调用urlencoded该方法 就表示要采用 application/x-www-form-urlencoded
+
+app.post('/formData',(req,res)=>{
+    // 创建formidable表单解析对象
+    const form = new formidable.IncomingForm();
+    // 解析客户端传递过来的Formdata对象
+    form.parse(req,(err,fields,files)=>{
+        res.send(fields);
+    })
+})
+
+app.listen(port, () => console.log(`服务器启动了`))
